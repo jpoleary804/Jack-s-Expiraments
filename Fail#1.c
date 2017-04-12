60cc2d83c4f2a94570189f66a094a13e51c0996e
jpoleary804 <jpoleary804@gmail.com>
2017-04-12 13:10:30 -0400

    Update Fail#1.c

--- Fail#1.c
+++ Fail#1.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 int main()
 {
-int input;
+float input;
+int output;
 input = 0;
 printf("Type temperature in Celcius to get it in Farenheit.\n");
 scanf("%d", input);
+output = ((9/5)*input)+32
+printf("%f Celcius is %f degrees Fareneheit", input, output);
+return 0;
+}
